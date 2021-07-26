961efa04c08d215b77bde232cff38f4964a0351b
Ash Blake <ash.blake@tutanota.com>
2021-07-26 20:17:53 +0200

    gale: dt2w: autodetect touchscreen event device
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- device.mk
+++ device.mk
@@ -127,6 +127,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DT2W-Service-Gale
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dt2w/dt2w_event:$(TARGET_COPY_OUT_VENDOR)/bin/dt2w_event
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
